d64df3ab437482f812c541575213872eca9a9e3f
Malvin Tok <kevenitetok@gmail.com>
2022-01-19 15:00:05 +0800

    Create program to output current time in GMT

---
+++ CurrentTime.java
@@ -0,0 +1,12 @@
+public class CurrentTime {
+    public static void main(String[] args) {
+        long totalMilliseconds = System.currentTimeMillis();
+        long totalSeconds = totalMilliseconds / 1000;
+        int currentSecond = (int)(totalSeconds % 60);
+        long totalMinutes = totalSeconds / 60;
+        int currentMinute = (int)(totalMinutes % 60);
+        long totalHours = totalMinutes / 60;
+        int currentHour = (int)(totalHours % 24);
+        System.out.printf("Current time is %d:%d:%d GMT\n", currentHour, currentMinute, currentSecond);
+    }
+}
